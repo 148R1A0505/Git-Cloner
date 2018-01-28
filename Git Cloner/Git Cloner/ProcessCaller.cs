900d23f4756983310a237682840fffe76b074ad0
sumanth.akkala <sumanth.akkala@GGKU4MPC62>
2018-01-28 12:29:27 +0530

    ProcessCalller class added.

---
+++ Git Cloner/Git Cloner/ProcessCaller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Cloner
+{
+    class ProcessCaller
+    {
+        internal string gitCall(string WorkDir, string args)
+        {
+            Process gitProcess = new Process();
+            var processStartInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WorkingDirectory = WorkDir,
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                Arguments = $"/c {args}"
+            };
+            gitProcess.StartInfo = processStartInfo;
+            gitProcess.Start();
+            string output = gitProcess.StandardOutput.ReadToEnd();
+            gitProcess.WaitForExit();
+            return output;
+        }
+    }
+}
